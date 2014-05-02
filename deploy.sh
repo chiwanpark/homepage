c9dea2748f3847ad8362308412b908d6151e689a
Chiwan Park <chiwanpark@icloud.com>
2014-05-03 04:51:59 +0900

    Github Token 변경 및 Token 노출 안되도록 수정

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git clone --quiet --branch=master https://${GH_TOKEN}@github.com/chiwanpark.github.io github
-cd github
+git clone --quiet --branch=master https://${GH_TOKEN}@github.com/chiwanpark/chiwanpark.github.io master > /dev/null
+cd master
 rsync -rv --exclude=.git ../_build/* .
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -fq origin master
+git push -fq origin master > /dev/null
 echo -e "Deploy completed."
