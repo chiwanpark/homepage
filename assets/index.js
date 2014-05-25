956cb03421743cf521efb1afe2b586530b1a3726
Chiwan Park <chiwanpark@icloud.com>
2014-05-25 13:57:23 +0000

    Travis build 6

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,17 @@
 jQuery(function ($) {
-    $('.fancybox').fancybox({
-        beforeShow: function () {
-            var alt = this.element.find('img').attr('alt');
-            this.inner.find('img').attr('alt', alt);
-            this.title = alt;
-        }
-    });
+  $('.fancybox').fancybox({
+    beforeShow: function () {
+      var alt = this.element.find('img').attr('alt');
+      this.inner.find('img').attr('alt', alt);
+      this.title = alt;
+    }
+  });
 
-    $('.fancybox-media').fancybox({
-        openEffect: 'none',
-        closeEffect: 'none',
-        helpers: {
-            media: {}
-        }
-    });
+  $('.fancybox-media').fancybox({
+    openEffect: 'none',
+    closeEffect: 'none',
+    helpers: {
+      media: {}
+    }
+  });
 });
