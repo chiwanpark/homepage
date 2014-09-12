14b00573e81f35db25d7b64264340d07b5f30854
Chiwan Park <chiwanpark@icloud.com>
2014-09-13 07:46:35 +0900

    rsync 수행 시 제거된 파일도 반영하도록 수정

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ fi
 
 git clone --quiet --branch=master https://${GH_TOKEN}@github.com/chiwanpark/chiwanpark.github.io master > /dev/null
 cd master
-rsync -rv --exclude=.git ../_build/* .
-git add -f .
+rsync -rv --delete --exclude=.git ../_build/* .
+git add -f --all .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push -fq origin master > /dev/null
 echo -e "Deploy completed."
