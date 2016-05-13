b3f1376852e927fc72fadcee3fe3aed3b9370340
Chiwan Park <chiwanpark@icloud.com>
2016-05-13 11:26:36 +0900

    Update Travis-CI configuration to build site based on v6 branch

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ fi
 
 git clone --quiet --branch=master https://${GH_TOKEN}@github.com/chiwanpark/chiwanpark.github.io master > /dev/null
 cd master
-rsync -rv --delete --exclude=.git ../_build/* .
+rsync -rv --delete --exclude=.git ../_build/ .
 git add -f --all .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push -fq origin master > /dev/null
