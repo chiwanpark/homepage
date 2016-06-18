9be8b9152d52a05b63295133779641dbaab36c83
Chiwan Park <chiwanpark@icloud.com>
2016-06-19 01:12:33 +0900

    Modify fixExternalLinks method to support my sites in external domain

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,26 @@
 (function () {
   'use strict';
 
+  var internalLinks = [
+    'localhost',
+    'chiwanpark.com',
+    'chiwanpark-status.azurewebsites.net'
+  ];
+
+  var checkInternalLink = function(link) {
+    for (var i = 0, length = internalLinks.length; i < length; ++i) {
+      if (link == internalLinks[i]) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
   var fixExternalLink = function () {
     var links = document.links;
     for (var i = 0, length = links.length; i < length; ++i) {
-      if (links[i].hostname != window.location.hostname) {
+      if (!checkInternalLink(links[i].hostname)) {
         links[i].target = '_blank';
       }
     }
